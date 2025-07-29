08c23395896f6326bad94f6be7e836ba1acc5cfc
karan radadiya <karanradadiya1132@gmail.com>
2025-07-29 11:26:03 +0530

    Add files via upload

---
+++ PR-Q5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+    int main() {
+     int i, j;
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= 5 - i; j++) {
+            printf("  "); 
+        }
+        for (j = 6 - i; j <= 5; j++) 
+		{
+            printf("%d ", j);
+        }
+        for (j = 4; j >= 6 - i; j--) {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+    }
+
+}
